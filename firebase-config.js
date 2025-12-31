8146e8772a3df5ebbedee930246b56f85b20f11c
Sayaz06 <adha86614@gmail.com>
2025-12-31 22:53:25 +0800

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCtxOE42D07yFc9eK3nLMsOy50SmeSErwI",
+  authDomain: "fatherstress-9e695.firebaseapp.com",
+  projectId: "fatherstress-9e695",
+  storageBucket: "fatherstress-9e695.firebasestorage.app",
+  messagingSenderId: "147729735248",
+  appId: "1:147729735248:web:f6771fee76727436a0fb55",
+  measurementId: "G-GF6HCJZ5MD"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
